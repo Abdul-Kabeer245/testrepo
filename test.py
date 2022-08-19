9a2d6c7674f3df543b4ce59d50144f1db121c9d5
Abdul-Kabeer245 <92785016+Abdul-Kabeer245@users.noreply.github.com>
2022-08-19 17:42:04 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>My Jupyter Notebook on IBM Watson Studio</h1>
+
+# <b>Abdul Kabeer</b>
+# <p>I am a Student and I want to be a Machine Learning Engineer</p>
+
+# <i>I am intrested in data science because I want to learn machine learning and join an MNC</i>
+
+# <h3>Code below will ask user for their name and print it with a hello (username) message. </h3>
+
+# In[3]:
+
+
+name = input("Enter your name: ")
+print("hello " + name)
+
+
+# <hr>
+# <ol>
+#     <li><a href = "https://youtu.be/vJs04-0nu3Y">video 1</li>
+#     <li><a href = "https://www.youtube.com/watch?v=Se9lh52BHno&t=345s">video 2</li>
+#     <li><a href = "https://www.youtube.com/watch?v=U1Brt2ID23M">video 3</li>
+# </ol>
+# <hr>
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
